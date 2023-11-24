536b9dfddc0ac3a3ac3e1b78b87d968b0b7e4006
MATZ Jérôme <JMatz@arfp.asso.fr>
2023-11-24 11:50:13 +0100

    Exercice_1.5

--- Algo/Java/mon1erprojet/src/Exercice_1_3/App.java
+++ Algo/Java/mon1erprojet/src/Exercice_1_3/App.java
@@ -4,26 +4,43 @@ import java.util.Scanner;
 
 public class App {
 
+	/* 
+           Exercice 1.3 Calcul de la surface d'un secteur circulaire
+
+Variable
+	rayonR est un entier 
+	angleA est un entier
+	aire est un reel
+Constante
+	pi est un reel
+Début du programme
+	Ecrire " Saisir le rayon R"
+	lire rayonR
+	Ecrire " Saisir l'angle a"
+	lire angleA
+	aire <-- (pi*rayonR^2*angleA)/360
+	ecrire "L'aire du secteur circulaire est : ",aire 
+fin du programme 
+ 
+ */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		// Déclaration des variables
 		int rayon; //est un entier
 		int angle; //est un entier
 		double aire; // est réel
 		
-		Scanner sc = new Scanner(System.in);
+		Scanner  sc = new Scanner(System.in);
 		
 		// Saisie du rayon
 		System.out.println("Saisir le rayon : ");
-		
 		rayon = sc.nextInt();
 		
 		// Saisie de l'angle
 		System.out.println("Saisir l'angle : ");
+		angle = sc.nextInt();
 		
 		// Calcul l'aire du secteur circulaire
-		aire = (Math.PI * rayon^2 * angle) / 360
+		aire = (Math.PI * Math.pow(rayon, 2) * angle) / 360;
 		
 		// Affichage de l'aire du secteur circulaire
 	    System.out.println("L'aire du secteur circulaire est : " + aire);
